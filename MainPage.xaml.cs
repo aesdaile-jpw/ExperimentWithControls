3f29a180a0643a4d556331de7aa222cdba892c36
Adrian Esdaile <adrian.esdaile@jpw.com.au>
2025-03-26 17:02:58 +1100

    page 108

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,19 +7,35 @@
         public MainPage()
         {
             InitializeComponent();
+
+            BirdPicker.ItemsSource = new string[]
+            {
+                "Duck",
+                "Pigeon",
+                "Sparrow",
+                "Owl",
+                "Parrot",
+                "Eagle",
+                "Hawk",
+                "Falcon",
+                "Vulture",
+                "Crow",
+                "Raven",
+                "Robin"
+            };
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
 
         private void Entry_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -34,6 +50,11 @@
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             SliderValue.Text = e.NewValue.ToString();
+        }
+
+        private void AddBird_Clicked(object sender, EventArgs e)
+        {
+            Birds.Text = Birds.Text + Environment.NewLine + BirdPicker.SelectedItem;
         }
     }
 
